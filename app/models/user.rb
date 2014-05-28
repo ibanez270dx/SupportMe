965f995a4fe24c028f4b617df57e1fdb5cd9df15
Jeff Miller <loadeddesigns@gmail.com>
2014-05-27 23:05:42 -0700

    Add validations to user model, add interests relation, add has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
-    
+  has_and_belongs_to_many :interests
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :phone, presence: true, uniqueness: true
+
+  has_secure_password
 end
