80301fdf11fd297a4893f7f2f4363f9c13a00b2d
Jeff Miller <loadeddesigns@gmail.com>
2014-05-27 23:06:03 -0700

    Add user helper methods to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
+  helper_method :current_user
+
+  def require_user
+    unless current_user
+      flash[:notice] = "You have to log in."
+      session[:user_id] = nil
+      redirect_to home_path and return false
+    end
+  end
+
 end
